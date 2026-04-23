827b38f32088cc18920b9f84bcd23792e8e97c82
KANAGALAKSHMI A <kanagaarun1808@gmail.com>
2026-04-23 14:55:09 +0530

    new updates 8

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,27 +2,28 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import Login from "./Login";
 import Game from "./Game";
+import Navbar from "./Navbar";
+
+function Leaderboard() {
+  return <h2>Leaderboard Page</h2>;
+}
+
+function Profile() {
+  return <h2>Profile Page</h2>;
+}
 
 function App() {
   const [userId, setUserId] = useState(localStorage.getItem("userId"));
 
   return (
     <BrowserRouter>
+      <Navbar />
+
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
 
-        {/* DEFAULT → LOGIN */}
-        <Route
-          path="/"
-          element={<Navigate to="/login" />}
-        />
+        <Route path="/login" element={<Login setUser={setUserId} />} />
 
-        {/* LOGIN */}
-        <Route
-          path="/login"
-          element={<Login setUser={setUserId} />}
-        />
-
-        {/* WORDLE (PROTECTED) */}
         <Route
           path="/wordle"
           element={
@@ -34,6 +35,8 @@ function App() {
           }
         />
 
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
